c10935d6d325ad55e4db43c0d0067dabb2c3ff93
Berkay Yılmaz <berkayyilmz01@gmail.com>
2023-12-17 23:09:33 +0300

    git

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
-const { mongo } = require('mongoose');
-var mongoose=require( 'mongoose' );
-var dbURI= 'mongodb://localhost/mekanbul';
+var mongoose=require('mongoose');
+var dbURI= "mongodb+srv://mekanbul32:sifre@mekanbul.lexttaq.mongodb.net/?retryWrites=true&w=majority";
+
 mongoose.connect(dbURI);
 mongoose.connection.on("connected",function(){
     console.log(dbURI+" Adresine bağlanıldı.");
@@ -8,12 +8,13 @@ mongoose.connection.on("connected",function(){
 mongoose.connection.on("error",function(){
     console.log(dbURI+" Adresine bağlanılamadı.");
 });
-mongoose.connection.on("disconneted",function(){
+mongoose.connection.on("disconnected",function(){
     console.log(dbURI+" Bağlantı kesildi!");
 });
 process.on("SIGINT",function(){
     mongoose.connection.close();
-    console.log("Uygulma kapatıldı.");
+    console.log("Uygulanpma kapatıldı.");
     process.exit(0);
     
 });
+require("./venues.js");
